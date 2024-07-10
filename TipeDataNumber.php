7d0466103171b584b5425b29aab3adc09c4b2cc5
agungruhiyat <agungruhiyat777@gmail.com>
2024-07-10 13:16:42 +0700

    Latihan 2

---
+++ TipeDataNumber.php
@@ -0,0 +1,28 @@
+<?php
+//Tipe Data Number//
+echo "Decimal : ";
+var_dump(1234);
+echo "Octal :";
+var_dump(01234);
+echo "Hexadecimal :";
+var_dump(0x1A);
+echo "Binary :";
+var_dump(0b111111);
+echo "Underscore in Number";
+var_dump(1_241_241_241);
+//Tipe Data Floating Point//
+echo "Floating Point :";
+var_dump(1.234);
+
+echo "Floating Point dengan E Notation Plus   (1.2 x1000) : ";
+var_dump(1.2e3);
+
+echo "Floating Point dengan E Notation Minu  ( 6 x 0.001) : ";
+var_dump(6e-3);
+
+echo " Underscore di Floating Point : ";
+var_dump(1_234.568);
+
+
+
+?>
