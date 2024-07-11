08961d48eda8bbe1c8970375d2ebc56b41e0f773
agungruhiyat <agungruhiyat777@gmail.com>
2024-07-11 09:55:52 +0700

    latihan 10

---
+++ MapArray.php
@@ -0,0 +1,15 @@
+$agung = array(
+"id" => "agung",
+"name" => "Agung Ruhiayat",
+"age" => 16
+
+);
+var_dump($agung);
+var_dump($agung["name"]);
+
+$nendra = [
+"id" => "nendar",
+"name" => "Nendra Saktiawan",
+"age" => 18
+];
+var_dump($nendra);
