6389bd94608c71778c5cbc8d36533277e0e1bf90
agungruhiyat <agungruhiyat777@gmail.com>
2024-07-22 07:47:52 +0700

    Latihan 11

---
+++ ArrayDalamArray.php
@@ -0,0 +1,15 @@
+<?php
+$agung = array(
+    "id"      => "agung",
+    "name"    => "Agung Ruhiayat",
+    "age"     => 16,
+    "address" => array(
+        "city"    => "Tasikmalaya",
+        "country" => "Indonesia"
+    )
+);
+var_dump($agung);
+var_dump($agung["name"]);
+var_dump($agung["address"]["country"]);
+
+?>
