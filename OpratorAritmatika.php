be5862365c6fcf47b6af1a7154b22ee6b67a55fb
agungruhiyat <agungruhiyat777@gmail.com>
2024-07-22 07:48:21 +0700

    Latihan 12

---
+++ OpratorAritmatika.php
@@ -0,0 +1,33 @@
+<?php
+$a = 82;
+$b = 50;
+
+$result = $a + $b;
+var_dump($result);
+
+$resultNEgative = -$result;
+var_dump($resultNEgative);
+
+$resultPositive = +$result;
+var_dump($resultPositive);
+
+//pengurangan//
+$result = $a - $b;
+var_dump($result);
+
+//perkalian//
+$result = $a * $b;
+var_dump($result);
+
+//pembagian//
+$result = $a / $b;
+var_dump($result);
+
+//perpangkatan//
+$result = $a ** $b;
+var_dump($result);
+
+//sisa bagi//
+$resultModulo = 100 % 3;
+var_dump($resultModulo);
+?>
